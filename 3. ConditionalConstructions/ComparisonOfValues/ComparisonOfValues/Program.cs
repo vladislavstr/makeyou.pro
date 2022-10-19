34016d5e84a36e1f7cd2f0a70fa2d0bbd16b9d5f
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2022-10-19 10:24:12 +0300

    3

---
+++ 3. ConditionalConstructions/ComparisonOfValues/ComparisonOfValues/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Пользователь вводит 2 числа (A и B). Если A больше B, подсчитать A+B, если A равно B, подсчитать A*B, если A меньше B, подсчитать A-B.
+
+Console.Write("A: ");
+int valueA = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("B: ");
+int valueB = Convert.ToInt32(Console.ReadLine());
+
+int valueC = 0;
+
+if (valueA > valueB)
+{
+    Console.Write("A > B ---> ");
+    Console.Write($"A + B: {valueA + valueB}");
+}
+
+if (valueA == valueB)
+{
+    Console.Write("A = B ---> ");
+    Console.Write($"A * B: {valueA * valueB}");
+}
+
+if (valueA < valueB)
+{
+    Console.Write("A < B ---> ");
+    Console.Write($"A - B: {valueA - valueB}");
+}
---
+++ 3. ConditionalConstructions/PasswordVerification/PasswordVerification/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Пользователь вводит в консоль пароль. Проверить совпадает ли он с "QWERTY123".
+
+Console.Write("write password and press enter/return: ");
+
+string password = Console.ReadLine();
+
+if (password != "QWERTY123")
+{
+    Console.WriteLine("Error");
+}
+else
+{
+    Console.WriteLine("Correct");
+}
