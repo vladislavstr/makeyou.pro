c601b5150e066872989dc17b14ae860badd6aa3c
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2022-10-24 00:22:07 +0300

    8

---
+++ Counter/Counter/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Пользователь вводит положительное число (A). Вывести сумму всех цифр у чисел из диапазона от 0 до A.
+
+Console.Write("write value : ");
+
+int value, crnt,tmp;
+
+value = Convert.ToInt32(Console.ReadLine());
+crnt = tmp = 0;
+
+while (crnt != value)
+{
+    crnt++;
+    tmp += crnt;
+
+}
+
+Console.WriteLine($"sum in range: {tmp}");
