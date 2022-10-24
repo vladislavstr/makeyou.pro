c3bddb68918efd48444d6d1336a855a7a4a68bfc
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2022-10-24 15:28:17 +0300

    9.1

--- 9.NestedLoops/CounterOfPrimeNumbers/CounterOfPrimeNumbers/Program.cs
+++ 9.NestedLoops/CounterOfPrimeNumbers/CounterOfPrimeNumbers/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+//Пользователь вводит 2 числа (A и B).
+//Вывести количество простых чисел (которые делятся только на 1 и самих себя) в диапазоне от A до B.
+
+
+Console.Write("bergin: ");
+int valueBg = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("end: ");
+int valueEnd = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+for (int i = valueBg; i <= valueEnd; i++)
+{
+    bool tmpFlag = true;
+
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            tmpFlag = false;
+            break;
+        }
+    }
+    if (tmpFlag)
+    {
+        count++;
+        //Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine(count);
---
+++ 9.NestedLoops/EqualNumber/EqualNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Пользователь вводит 2 числа. Сообщите, есть ли в написании двух чисел одинаковые цифры.
+//Например, для пары 123 и 345, ответом будет являться “ДА”, а, для пары 500 и 999 - “НЕТ”.
+
+Console.Write("Value A: ");
+int valueA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Value B: ");
+int valueB = Convert.ToInt32(Console.ReadLine());
+
+
+int numB, numA, count;  
+numA = numB = count = 0;
+
+while (valueA !=0)
+{
+
+    numA = valueA % 10;
+    valueA /= 10;
+
+    int tmpValB = valueB;
+
+    while (tmpValB != 0)
+    {
+
+        numB = tmpValB % 10;
+        tmpValB /= 10;
+
+        if (numA == numB)
+        {
+
+            count++;
+            
+        }
+    }
+}
+if (count > 0)
+{
+    Console.WriteLine($"Да, {count} знак/a");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
