0be1a78e060bab19d606a8bba58a359aa221419a
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2022-11-02 20:40:27 +0300

    14

---
+++ 14.ClassMembers/ZooResidents/Animals.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace ZooResidents;
+
+//Животное в зоопарке описывается на основе следующих параметров:
+public class Animals
+{
+    //Свойства класса
+    //Общее описание вида:
+
+    //Описание конкретного животного:
+    //Имя(Петя, Симба, Матильда)
+    //private string _name;
+    public string Name { get; set; }
+
+    //Необходимый биом(Тропики, Тундра, Пустыня)
+    //private string _type;
+    public string Type { get; set; }
+
+    //Необходимая площадь на особь(10 м^2)
+    //private string _square;
+    public string Square { get; set; }
+
+    //Что кушает(Рыба, Мясо, Сено)
+    //private string _ration;
+    public string Ration { get; set; }
+
+    //Хищник или травоядное
+    //private string _predatorOrHerbivore;
+    public string PredatorOrHerbivore { get; set; }
+
+    //Голоч (Ауф, Хрю, Рррр)
+    //private string _voice;
+    public string Voice { get; set; }
+
+    //Объем потребляемой в сутки еды (5кг, 0.3кг)
+    //private string _appetite;
+    public double Appetite { get; set; }
+
+    //        Возраст(5, 1, 20)
+    private int _age;
+    public int Age
+    {
+        get
+        {
+            return _age;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                _age = value;
+            }
+        }
+    }
+
+
+    //  Метод класса
+    //Есть
+    public void ToEat()
+    {
+        Console.WriteLine($"{Type} {Name} eating {Ration}");
+    }
+
+
+    //Издать звук
+    public void DoVoice()
+    {
+        Console.WriteLine($"{Type} {Name} make sounds: {Voice}");
+    }
+
+    //Играть
+    public void Play()
+    {
+        Console.WriteLine($"{Type} {Name} is play");
+    }
+}
+
---
+++ 14.ClassMembers/ZooResidents/Program.cs
@@ -0,0 +1,37 @@
+﻿using ZooResidents;
+
+Animals Animal_1 = new Animals();
+
+Animal_1.Type = "Слон";
+Animal_1.Name = "Петя";
+Animal_1.Voice = "Ауф";
+Animal_1.Ration = "Сено";
+Animal_1.ToEat();
+Animal_1.DoVoice();
+Animal_1.Play();
+Console.WriteLine();
+
+
+Animals Animal_2 = new Animals();
+
+Animal_2.Type = "Пингвин";
+Animal_2.Name = "Симба";
+Animal_2.Voice = "Хрю";
+Animal_2.Ration = "Рыба";
+Animal_2.ToEat();
+Animal_2.DoVoice();
+Animal_2.Play();
+Console.WriteLine();
+
+
+Animals Animal_3 = new Animals();
+
+Animal_3.Type = "Тигр";
+Animal_3.Name = "Матильда";
+Animal_3.Voice = "Рррр";
+Animal_3.Ration = "Мясо";
+Animal_3.ToEat();
+Animal_3.DoVoice();
+Animal_3.Play();
+Console.WriteLine();
+
